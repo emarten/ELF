25156abef85be038ddb53d36469ac23c509e9c78
e.marten <e.marten@eric-bank.testroom.gruebler.dyndns.org>
2014-01-16 19:44:49 +0100

    test

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
   $_ELF=array(
     'init'=>__FILE__
   );
-  
+
   include(dirname(__FILE__)."/elf-runtime/inc.php");
 
-  echo "<hr />";  
+  echo "<hr />";
   echo "<pre>".print_r($_ELF,true)."</pre>";
- 
+
+/*
+ *
+ * lalala
+ */
